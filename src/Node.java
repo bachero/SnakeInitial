ab607550dc9e4e0ba5ed79e44ff68d0ee6e19b1e
user <user@192.168.1.135>
2020-04-11 19:09:42 +0200

    Repeat commit, because Node not upload.

--- src/Node.java
+++ src/Node.java
@@ -13,4 +13,24 @@ public class Node {
     private int col;
     
     // Create the constructor and all the methods you need here
+    public Node(int r, int c){
+        row = r;
+        col = c;
+    }
+    
+    public int getRow(){
+        return row;
+    }
+    
+    public int getCol(){
+        return col;
+    }
+    
+    public void setRow(int r){
+        row = r;
+    }
+    
+    public void setCol(int c){
+        col = c;
+    }
 }
