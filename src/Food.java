f9b9b8bf5c631043fba393e980ae885a18628e6e
user <user@192.168.1.135>
2020-04-23 20:46:47 +0200

    random generateFood in class food

--- src/Food.java
+++ src/Food.java
@@ -1,5 +1,6 @@
 
 import java.awt.Graphics;
+import java.util.List;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,7 +19,28 @@ public class Food {
     public Food(Snake snake) { 
         // We pass Snake to the constructor because if the randomnly generated food
         // falls on the Snake you have to create another position for the food
-        
+        position = generateFood(snake);
+    }
+    
+    public Node generateFood(Snake snake){
+        position = new Node((int)(Math.random()*(Board.NUM_ROWS -1)), (int)(Math.random()*(Board.NUM_COLS -1)));
+        List<Node> posSnake = snake.getList();
+        boolean colision = false;
+        boolean notColision = true;
+        Node food = null;
+        while(!colision){    
+            for(Node a: posSnake){
+                if(a == position){
+                    notColision = false;
+                    break;
+                }
+            }
+            colision = true;
+        }
+        if(notColision){
+            food = position;
+        }
+        return food;
     }
     
     public void paint(Graphics g, int squareWidth, int squareHeight) {
